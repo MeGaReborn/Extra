bdc9c5702f4fa23e605ccf941967392574db3dde
bibak <piluxada@apkmd.com>
2017-01-28 11:16:44 -0800

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -22,7 +22,7 @@ local function plugin_exists( name )
 end
 
 local function list_all_plugins(only_enabled)
-  local tmp = check_markdown('\n\n@TurboCh')
+  local tmp = check_markdown('\n\n@BanG_TeaM')
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
