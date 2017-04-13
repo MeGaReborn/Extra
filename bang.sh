bdc09ef2597bbba0eea1243321f636ef57bb7efb
boydev <rezabayat594@gmail.com>
2017-04-13 19:15:10 +0430

    Update bang.sh

--- bang.sh
+++ bang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/Bang-TG
+cd $HOME/Extra
 
 install() {
 	    cd tg
@@ -31,7 +31,7 @@ if [ ! -f ./tg/tgcli ]; then
     exit 1
 fi
    echo -e "\033[38;5;208m"
-   echo -e "     > BanG TeaM :D                  "
+   echo -e "     > @BOTREBORN_CH :D                  "
    echo -e "                                              \033[0;00m"
    echo -e "\e[36m"
    ./tg/tgcli -s ./bot/bot.lua $@
